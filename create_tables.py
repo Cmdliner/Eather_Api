2834fa71da83925d86779bf1f77c7d3e8763d459
Adeyemi Abiade <abiadeabdulazeez@gmail.com>
2024-08-06 23:02:17 +0100

    Modified create_tables script

--- create_tables.py
+++ create_tables.py
@@ -2,12 +2,25 @@ from config.db import engine
 from models.appointment import Base as AppointmentBase
 from models.user import Base as UserBase
 from models.test import Base as TestBase
+from models.association import appointment_tests  # Import the association table
 
 
 def create_tables():
-    UserBase.metadata.create_all(bind=engine)
-    TestBase.metadata.create_all(bind=engine)
-    AppointmentBase.metadata.create_all(bind=engine)
+    # Create a new MetaData object
+    from sqlalchemy import MetaData
+
+    combined_metadata = MetaData()
+
+    # Combine the metadata from all your models
+    for base in (UserBase, TestBase, AppointmentBase):
+        for table in base.metadata.tables.values():
+            table.tometadata(combined_metadata)
+
+    # Add the association table to the combined metadata
+    appointment_tests.tometadata(combined_metadata)
+
+    # Create all tables
+    combined_metadata.create_all(bind=engine)
 
 
 if __name__ == "__main__":
