5685902f56ef72870db089e1e7e293ea31373164
Adeyemi Abiade <abiadeabdulazeez@gmail.com>
2024-08-28 18:13:08 +0100

    Added appointment history and details actions

--- api/appointment.py
+++ api/appointment.py
@@ -1,6 +1,7 @@
 from config.db import get_db
 from datetime import datetime, UTC
 from fastapi import APIRouter, Depends, status, HTTPException, Request
+from middlewares.auth import require_auth
 from models.appointment import Appointment
 from models.user import User
 from models.test import Test
@@ -9,15 +10,15 @@ from sqlalchemy.orm import Session
 from utils.auth_helpers import get_current_user
 
 
-router = APIRouter()
+router = APIRouter(dependencies=[Depends(require_auth)])
 
 
 @router.post("/create", status_code=status.HTTP_201_CREATED)
-def new_appointment(
+async def new_appointment(
     appointment: AppointmentSchema, req: Request, db: Session = Depends(get_db)
 ):
     try:
-        authToken = req.headers.get('Authorization').split(' ')[-1]
+        authToken = req.headers.get("Authorization").split(" ")[-1]
         user_email = get_current_user(authToken)
         print(user_email)
         user = db.query(User).filter(User.email == user_email).first()
@@ -46,8 +47,51 @@ def new_appointment(
     except Exception as err:
         print(err)
         raise HTTPException(detail="Internal server error", status_code=500)
-    
 
-@router.get('/{id}/details')
-def get_appointment_details():
-    pass
+
+@router.get("/{appointment_id}/details")
+async def get_appointment_details(appointment_id: str, db: Session = Depends(get_db)):
+    try:
+        appointment = (
+            db.query(Appointment).filter(Appointment.id == appointment_id).first()
+        )
+        print(f"Apointment -> {appointment}")
+        if appointment is None:
+            raise HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND, detail="Appointment not found"
+            )
+        return appointment
+    except Exception as err:
+        print(err)
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail="Error getting appointment details",
+        )
+
+
+@router.get("/my-appointments")
+async def get_all_user_appointments(req: Request, db: Session = Depends(get_db)):
+    try:
+        authToken = req.headers.get("Authorization").split(" ")[-1]
+        if authToken is None:
+            raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED, detail="Unauthorized"
+            )
+        user_email = get_current_user(authToken)
+        user = db.query(User).filter(User.email == user_email).first()
+
+        if user is None:
+            raise HTTPException(
+                status_code=status.HTTP_403_FORBIDDEN, detail="Unauthorized"
+            )
+
+        user_appointments = (
+            db.query(Appointment).filter(Appointment.user_id == user.id).all()
+        )
+        return user_appointments
+    except Exception as err:
+        print(err)
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail="Error getting user appointments",
+        )
